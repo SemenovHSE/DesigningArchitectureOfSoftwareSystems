dc2adcf5705c127298c44fcc316c7c87e10013ac
SemenovHSE <31741736+SemenovHSE@users.noreply.github.com>
2020-02-18 09:47:44 +0500

    Добавлен проект с 4 лабораторной работой.

---
+++ Patterns/Composite/Composite/Composite.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite.Composite
+{
+    public interface Food
+    {
+        string Name { get; }
+        int GetMicronutrients();
+        string ShowComposition();
+    }
+    public class Ingredient : Food
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        private int micronutrients;
+        public Ingredient(string _name, int _micronutrients)
+        {
+            name = _name;
+            micronutrients = _micronutrients;
+        }
+        public int GetMicronutrients()
+        {
+            return micronutrients;
+        }
+        public string ShowComposition()
+        {
+            return name;
+        }
+    }
+    public class Dish : Food
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        private List<Food> parts;
+        public List<Food> Parts
+        {
+            get
+            {
+                return parts;
+            }
+            private set
+            {
+                parts = value;
+            }
+        }
+        public Dish(string _name)
+        {
+            name = _name;
+            parts = new List<Food>();
+        }
+        public Dish(string _name, List<Food> _parts)
+        {
+            name = _name;
+            parts = _parts;
+        }
+        public void Add(Food part)
+        {
+            parts.Add(part);
+        }
+        public void Remove(Food part)
+        {
+            parts.Remove(part);
+        }
+        public int GetMicronutrients()
+        {
+            int micronutrients = 0;
+            foreach (Food part in parts)
+            {
+                micronutrients += part.GetMicronutrients();
+            }
+            return micronutrients;
+        }
+        public string ShowComposition()
+        {
+            string result = "";
+            bool first = true;
+            foreach (Food part in parts)
+            {
+                if (first)
+                {
+                    result += part.ShowComposition();
+                    first = false;
+                    continue;
+                }
+                result += " ";
+                result += part.ShowComposition();
+            }
+            return result;
+        }
+    }
+}
---
+++ Patterns/Composite/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Composite.Composite;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dish puff = new Dish("Слойка с фруктами");
+            Dish dough = new Dish("Бездрожжевое слоёное тесто");
+            Ingredient flour = new Ingredient("Мука", 10);
+            Ingredient butter = new Ingredient("Сливочное масло", 70);
+            Ingredient salt = new Ingredient("Соль", 3);
+            dough.Add(flour);
+            dough.Add(butter);
+            dough.Add(salt);
+            Ingredient fruits = new Ingredient("Фрукты", 100);
+            Ingredient sugar = new Ingredient("Сахар", 15);
+            Ingredient sourCream = new Ingredient("Сметана", 50);
+            puff.Add(dough);
+            puff.Add(fruits);
+            puff.Add(sugar);
+            puff.Add(sourCream);
+
+            Console.WriteLine("Состав слойки с фруктами:");
+            Console.WriteLine(puff.ShowComposition());
+            Console.WriteLine("Количество полезных микроэлементов в слойке с фруктами:");
+            Console.WriteLine(puff.GetMicronutrients());
+
+            Console.WriteLine("Состав бездрожжевого слоёного теста:");
+            Console.WriteLine(dough.ShowComposition());
+            Console.WriteLine("Количество полезных микроэлементов в бездрожжевом слоёном тесте:");
+            Console.WriteLine(dough.GetMicronutrients());
+
+            Console.WriteLine("Состав фруктов:");
+            Console.WriteLine(fruits.ShowComposition());
+            Console.WriteLine("Количество полезных микроэлементов во фруктах:");
+            Console.WriteLine(fruits.GetMicronutrients());
+        }
+    }
+}
