afd64ccd1eba18b526db43351e9374ad0c92989f
SemenovHSE <31741736+SemenovHSE@users.noreply.github.com>
2020-03-09 17:36:37 +0500

    Добавлен проект с 8 лабораторной работой.

---
+++ Patterns/Interpreter/Interpreter/Interpreter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter.Interpreter
+{
+    public class Context
+    {
+        private string input;
+        public string HexCode
+        {
+            get
+            {
+                return input;
+            }
+            set
+            {
+                input = value;
+            }
+        }
+        private string output;
+        public string BinaryCode
+        {
+            get
+            {
+                return output;
+            }
+            set
+            {
+                output = value;
+            }
+        }
+        public Context(string code)
+        {
+            HexCode = code;
+        }
+    }
+
+    interface Expression
+    {
+        void Interpret(Context context);
+    }
+
+    public class DigitExpression : Expression
+    {
+        private string[] digitCodes = new string[] { "0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001" };
+        public void Interpret(Context context)
+        {
+            char currentDigit = context.HexCode[0];
+            string digitBinaryCode = digitCodes[currentDigit - '0'];
+            context.BinaryCode += digitBinaryCode;
+            context.HexCode = context.HexCode.Remove(0, 1);
+        }
+    }
+
+    public class LetterExpression : Expression
+    {
+        private Dictionary<char, string> letterCodes = new Dictionary<char, string>()
+        {
+            ['A'] = "1010",
+            ['B'] = "1011",
+            ['C'] = "1100",
+            ['D'] = "1101",
+            ['E'] = "1110",
+            ['F'] = "1111"
+        };
+        public void Interpret(Context context)
+        {
+            char currentLetter = context.HexCode[0];
+            string letterBinaryCode = letterCodes[currentLetter];
+            context.BinaryCode += letterBinaryCode;
+            context.HexCode = context.HexCode.Remove(0, 1);
+        }
+    }
+}
---
+++ Patterns/Interpreter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interpreter.Interpreter;
+
+namespace Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hexCodeToInterpret = "C71A4F3B";
+            Context context = new Context(hexCodeToInterpret);
+            List<Expression> interpretationTree = new List<Expression>()
+            {
+                new LetterExpression(),
+                new DigitExpression(),
+                new DigitExpression(),
+                new LetterExpression(),
+                new DigitExpression(),
+                new LetterExpression(),
+                new DigitExpression(),
+                new LetterExpression()
+            };
+            foreach (Expression expression in interpretationTree)
+            {
+                expression.Interpret(context);
+            }
+            Console.WriteLine(hexCodeToInterpret + " в шестнадцатиричной системе счисления = " + context.BinaryCode + " в двоичной системе счисления.");
+        }
+    }
+}
