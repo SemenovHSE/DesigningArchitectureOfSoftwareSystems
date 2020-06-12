3a9f035c352ba260bac55cd005c360964ba3225d
SemenovHSE <31741736+SemenovHSE@users.noreply.github.com>
2020-06-12 14:22:40 +0500

    Добавлен проект с 9 лабораторной работой.

---
+++ Patterns/Observer/Observer/Observer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer.Observer
+{
+    public interface Observable
+    {
+        void AddObserver(Observer observer);
+        void RemoveObserver(Observer observer);
+        void NotifyObservers();
+    }
+
+    public interface Observer
+    {
+        void CheckDashboard(Information information);
+    }
+
+    public class Analyst : Observer
+    {
+        private string fullName;
+
+        public string FullName
+        {
+            get
+            {
+                return fullName;
+            }
+            set
+            {
+                fullName = value;
+            }
+        }
+
+        public Analyst(string _fullName)
+        {
+            FullName = _fullName;
+        }
+        
+        public void CheckDashboard(Information information)
+        {
+            Console.WriteLine(FullName + " " + "Проверка дашборда!");
+            information.ShowDashboardData();
+        }
+    }
+
+    public class Dashboard : Observable
+    {
+        private List<Observer> observers;
+
+        public Dashboard()
+        {
+            observers = new List<Observer>();
+        }
+
+        public void AddObserver(Observer observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(Observer observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            List<string> data = new List<string>()
+            {
+                "Температура",
+                "Влажность",
+                "Электропотребление"
+            };
+            Information information = new Information(data);
+            foreach (Observer observer in observers)
+            {
+                observer.CheckDashboard(information);
+            }
+        }
+    }
+
+    public class Information
+    {
+        private List<string> dashboardData;
+
+        public List<string> DashboardData
+        {
+            get
+            {
+                return dashboardData;
+            }
+            set
+            {
+                dashboardData = value;
+            }
+        }
+
+        public Information()
+        {
+            DashboardData = new List<string>();
+        }
+
+        public Information(List<string> data)
+        {
+            DashboardData = data;
+        }
+
+        public void ShowDashboardData()
+        {
+            string result = "";
+            for (int i = 0; i < DashboardData.Count; i++)
+            {
+                result += DashboardData[i];
+                if (i != DashboardData.Count - 1)
+                {
+                    result += ", ";
+                }
+            }
+            Console.WriteLine("Информация с дашборда: " + result);
+        }
+    }
+}
---
+++ Patterns/Observer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Observer.Observer;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Analyst analyst1 = new Analyst("Аналитик1");
+            Analyst analyst2 = new Analyst("Аналитик2");
+            Analyst analyst3 = new Analyst("Аналитик3");
+            Analyst analyst4 = new Analyst("Аналитик4");
+            Dashboard dashboard = new Dashboard();
+            dashboard.AddObserver(analyst1);
+            dashboard.AddObserver(analyst2);
+            dashboard.AddObserver(analyst3);
+            dashboard.AddObserver(analyst4);
+            dashboard.NotifyObservers();
+            dashboard.RemoveObserver(analyst2);
+            dashboard.NotifyObservers();
+        }
+    }
+}
